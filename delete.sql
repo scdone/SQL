551e76adbe441396a9e24691e4792ad1e8d382d3
Stetson Done <scdone@gmail.com>
2022-01-03 13:56:34 -0700

    delete done

--- delete.sql
+++ delete.sql
@@ -1,3 +1,11 @@
+-- CREATE TABLE animals (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(128),
+--     type VARCHAR(128),
+--     age INT
+-- )
+
+
 -- INSERT INTO animals ( name, type, age ) 
 -- VALUES ('Leo', 'lion', 12),
 -- ('Jerry', 'mouse', 4),
@@ -8,3 +16,21 @@
 -- ('Nala', 'lion', 2),
 -- ('Marie', 'cat', 1),
 -- ('Flounder', 'fish', 8);
+
+-- SELECT * FROM animals
+
+-- DELETE FROM animals 
+-- WHERE type = 'lion'
+
+-- SELECT * FROM animals
+
+
+-- DELETE FROM animals 
+-- WHERE name LIKE 'M%'
+
+-- SELECT * FROM animals 
+
+-- DELETE FROM animals 
+-- WHERE age < 9
+
+-- SELECT * FROM animals
