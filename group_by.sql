4a4db3a7b34b16dd4895be406b3ec81d0c2c699f
Stetson Done <scdone@gmail.com>
2022-01-03 12:34:05 -0700

    group by done

---
+++ group_by.sql
@@ -0,0 +1,9 @@
+SELECT SUM(total) FROM invoice 
+GROUP BY billing_state
+
+SELECT AVG(milliseconds) FROM track
+GROUP BY album_id
+
+SELECT COUNT(album_id) FROM album 
+WHERE artist_id IN (8, 22)
+GROUP BY artist_id
